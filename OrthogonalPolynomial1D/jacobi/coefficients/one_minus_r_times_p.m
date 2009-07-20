393b3eeb633e1c0de35d7014f788954b73026de5
Akil Narayan <anaray@dam.brown.edu>
2009-07-20 14:07:56 -0400

    (1 \pm r) x p connection coefficients

---
+++ OrthogonalPolynomial1D/jacobi/coefficients/one_minus_r_times_p.m
@@ -0,0 +1,33 @@
+function[mu] = one_minus_r_times_p(n,alpha,beta,varargin)
+% [MU] = ONE_MINUS_R_TIMES_P(N,ALPHA,BETA,{NORMALIZATION='monic'})
+%
+%     Computes the connection coefficients between (1-r) x P and P. I.e., given
+%     the vector N of whole-number indices, and valid Jacobi class parameters
+%     ALPHA and BETA, MU is a length(N) x 2 matrix, where each row contains the
+%     coefficients defined by the relation:
+%
+%     (1-r)*P^(alpha,beta)_n = mu(1)*P^(alpha-1,beta)_n +
+%                              mu(2)*P^(alpha-1,beta)_{n+1}
+%
+%     The optional input NORMALIZATION defines the normalization of the
+%     polynomials P, which affects the values of the parameters MU.
+
+global handles;
+opt = handles.common.InputSchema({'normalization'}, {'normal'}, [],varargin{:});
+n = n(:);
+N = length(n);
+
+mu = zeros([N,2]);
+
+switch opt.normalization
+case 'normal'
+  mu(:,1) = sqrt(2*(n+alpha).*(n+alpha+beta)./...
+            ((2*n+alpha+beta).*(2*n+alpha+beta+1)));
+
+  mu(:,2) = -sqrt(2*(n+1).*(n+beta+1)./...
+            ((2*n+alpha+beta+1).*(2*n+alpha+beta+2)));
+otherwise
+  fprintf('Error: normalization %s not supported', opt.normalization);
+  mu = false;
+  return
+end
---
+++ OrthogonalPolynomial1D/jacobi/coefficients/one_plus_r_times_p.m
@@ -0,0 +1,31 @@
+function[mu] = one_plus_r_times_p(n,alpha,beta,varargin)
+% [MU] = ONE_PLUS_R_TIMES_P(N,ALPHA,BETA,{NORMALIZATION='monic'})
+%
+%     Computes the connection coefficients between (1+r) x P and P. I.e., given
+%     the vector N of whole-number indices, and valid Jacobi class parameters
+%     ALPHA and BETA, MU is a length(N) x 2 matrix, where each row contains the
+%     coefficients defined by the relation:
+%
+%     (1+r)*P^(alpha,beta)_n = mu(1)*P^(alpha,beta-1)_n +
+%                              mu(2)*P^(alpha,beta-1)_{n+1}
+%
+%     The optional input NORMALIZATION defines the normalization of the
+%     polynomials P, which affects the values of the parameters MU.
+
+global handles;
+coeffs = handles.speclab.OrthogonalPolynomial1D.jacobi.coefficients;
+opt = handles.common.InputSchema({'normalization'}, {'normal'}, [],varargin{:});
+n = n(:);
+N = length(n);
+
+mu = zeros([N,2]);
+
+switch opt.normalization
+case 'normal'
+  mu = coeffs.one_minus_r_times_p(n,beta,alpha,opt);
+  mu(:,2) = -mu(:,2);
+otherwise
+  fprintf('Error: normalization %s not supported', opt.normalization);
+  mu = false;
+  return
+end
