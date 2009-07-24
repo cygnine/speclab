222ad8e1811758db56f3628112e55d485580e263
Akil Narayan <anaray@dam.brown.edu>
2009-07-24 11:06:09 -0400

    Fourier fft/ifft files added

--- fourier/fft/fft.m
+++ fourier/fft/fft.m
@@ -1,22 +1,21 @@
 function[modes] = fft(nodes,varargin);
-% [MODES] = FFT(NODES,G,D);
+% [MODES] = FFT(NODES,{GAMMA=0,DELTA=0,SHIFT=0,SCALE=1});
 %
 %     A wrapper for Matlab's FFT. This function scales the modes so that they
 %     correspond to modal coefficients in the basis expansion
 %     1/sqrt(2*pi)*exp(i*k*theta), where k is an integer index, negatively
-%     biased when N = length(modes) is even. If the inputs G or D are greater
-%     than 0 and are natural numbers, this function also performs the connection
-%     problem so that the output is the modes for the (G,D) generalized Fourier
-%     expansion.
+%     biased when N = length(modes) is even. If the inputs GAMMA or DELTA are
+%     greater than 0 and are natural numbers, this function also performs the
+%     connection problem so that the output is the modes for the (GAMMA,DELTA)
+%     generalized Fourier expansion.
 
 global handles;
-opt = handles.common.InputSchema({'G','D'},{0,0},[],varargin{:});
-
-fourier = handles.speclab.fourier;
+opt = handles.common.InputSchema({'G','D','shift','scale'},{0,0,0,1},[],varargin{:});
+conn = handles.speclab.fourier.connection.positive_integer_separation_connection;
 
 modes = fft(nodes);
 modes = fftshift(modes)/length(modes)*sqrt(2*pi);
 
-if (opt.G+opt.D)>0
-  modes = fourier.connection.integer_separation_connection(modes,0,0,opt.G,opt.D);
+if (opt.gamma+opt.delta)>0
+  modes = conn(modes,0,0,opt.gamma,opt.delta);
 end
--- fourier/fft/ifft.m
+++ fourier/fft/ifft.m
@@ -1,9 +1,22 @@
-function[modes] = matlab_modes_pre_ifft(modes);
-% [MODES] = MATLAB_MODES_PRE_IFFT(MODES);
+function[nodes] = ifft(modes);
+% [NODES] = IFFT(MODES,{GAMMA=0,DELTA=0,SHIFT=0,SCALE=1})
 %
-%     The inverse of MATLAB_FFT_TO_MODES. Prepares the modal coefficients
-%     corresponding to the basis expansion 1/sqrt(2*pi)*exp(i*k*theta) so that
-%     they can be input into matlab's ifft to give back nodal evaluations at
-%     equidistant points.
+%     A wrapper for Matlab's IFFT. For GAMMA=DELTA=0, takes in modal
+%     coefficients corresponding to basis functions 1/sqrt(2*pi)*exp(i*k*theta)
+%     and returns the nodal evaluations at the canonical Fourier nodal
+%     locations. 
+%
+%     The inputs GAMMA and DELTA specify the type of generalized Fourier
+%     expansion, and the values SHIFT and SCALE dictate the affine map from
+%     [-pi,pi] to another interval.
+
+global handles;
+opt = handles.common.InputSchema({'gamma','delta','shift','scale'},{0,0,0,1},[],varargin{:});
+conn = handles.speclab.fourier.connection.negative_integer_separation_connection;
+
+if (opt.gamma+opt.delta)>0
+  modes = conn(modes,0,0,opt.gamma,opt.delta)
+end
 
 modes = ifftshift(modes)*length(modes)/sqrt(2*pi);
+nodes = ifft(modes);
