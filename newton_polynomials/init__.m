36561f81ac0c32b884d867dde0648518b876f022
Akil Narayan <akil@purdue.edu>
2012-03-01 14:13:05 -0500

    Added init__ file for newton_polynomials

---
+++ newton_polynomials/init__.m
@@ -0,0 +1,10 @@
+function[newton_polynomials] = init__()
+% init__ -- Initialization file for speclab/newton_polynomials module
+%
+% [nodes] = init__()
+
+module_list = {};
+
+newton_polynomials.module_list = module_list;
+newton_polynomials.recurse_files = true;
+newton_polynomials.addpaths = {};
